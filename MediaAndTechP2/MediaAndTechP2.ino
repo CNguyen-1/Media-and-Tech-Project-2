febf02fab369c503a05ce8177568094e0c35ac11
Christine Nguyen <cmnguyen0101@gmail.com>
2022-04-07 14:52:58 -0500

    Created variables and initialized servo and button pins

--- MediaAndTechP2/MediaAndTechP2.ino
+++ MediaAndTechP2/MediaAndTechP2.ino
@@ -1,6 +1,19 @@
+/*
+ AET Little Mermaid Enclosure
+ Team Name: Floundering Flounders
+ Team Members: Airi Hibler, Bianca Jimenez, Christine Nguyen 
+ Description: button and servo program
+ */
+
+#include<Servo.h>;
+
+//VARIABLES
+Servo myServo;
+const int buttonPin = 7;
 void setup() {
   // put your setup code here, to run once:
-
+  myServo.attach(9);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
