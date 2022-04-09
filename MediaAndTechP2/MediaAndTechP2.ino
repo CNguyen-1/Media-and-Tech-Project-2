ccd826e79e6b429792ac1949c1fbb2aecac75f35
Christine Nguyen <cmnguyen0101@gmail.com>
2022-04-09 02:43:48 -0500

    Planning out if/else statements and variables

--- MediaAndTechP2/MediaAndTechP2.ino
+++ MediaAndTechP2/MediaAndTechP2.ino
@@ -7,21 +7,76 @@
 
 #include<Servo.h>;
 
-//VARIABLES
-Servo myServo;
-const int buttonPin = 7;
+//VARIABLES 
+Servo ursulaServo;
+Servo whirlServo;
+Servo armServo;
+
+//variables values to be assigned later;
+const int ursulaSwitch = ; 
+const int whirlButton = ;
+const int armSwitch = ;
+const int boatSwitch = ;
+const int tridentSwitch = ;
+const int tridentLED = ; 
+const int boatLED = ;
+
+
 void setup() {
   // put your setup code here, to run once:
-  myServo.attach(9);
-  pinMode(buttonPin, INPUT);
+  ursulaServo.attach(9);//might need to change numbers
+  whirlServo.attach(9);
+  armServo.attach(9);
+  pinMode(ursulaSwitch, INPUT);
+  pinMode(whirlButton, INPUT);
+  pinMode(tridentSwitch, INPUT); //may change variable name
+  pinMode(boatSwitch, INPUT);
+  pinMode(armSwitch, INPUT);
+  pinMode(tridentLED, OUTPUT);
+  pinMode(boatLED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // if button is pressed, swivel servo 180 degrees
-  if(digitalRead(buttonPin) == HIGH){
-    myServo.write(180);
+
+  //1.Ursula flipping to reveal transformed Ursula
+  if(digitalRead(ursulaSwitch) == HIGH){
+    ursulaServo.write(180);
   } else {
-    myServo.write(0);
+    ursulaServo.write(0);
+  }
+
+  //2.Whirlpool spinning
+  if(digitalRead(whirlButton) == HIGH){
+    whirlServo.write(180);
+    whirlServo.write(180); //check if this gives a spinning effect
+  } else {
+    whirlServo.write(0);
+  }
+
+  //3.Trident activates LED
+  if(digitalRead(tridentSwitch) == HIGH){
+    digitalWrite(tridentLED, HIGH);
+  } else {
+    digitalWrite(tridentLED, LOW);
+  }
+
+  //4.Boat switch activates LEDs/LED?, maybe make it flickering
+  if(digitalRead(boatSwitch == HIGH){
+    digitalWrite(boatLED, HIGH);
+    delay(500); //delay time may need to be changed to get desired effect
+    digitalWrite(boatLED, LOW);
+    delay(500)
+  } else {
+    digitalWrite(boatLED, LOW);
+  }
+
+  //5.Arm switch (cake object) activates servo on Ariel's arm to have it wave
+  if(digitalRead(armSwitch) == HIGH){
+    armServo.write(180); //check if it gives a waving effect
+    armServo.write(180);
+  } else {
+    armServo.write(0);
   }
 }
